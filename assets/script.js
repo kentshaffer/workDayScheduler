cb485c3070352f34ac9e060e88f596c15ea16f6a
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-07-19 17:29:13 -0700

    text printing to textarea and staying after page has been refreshed

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var dateDisplayEl = $('#currentDay');
 var timeSlot = $(this).parent().siblings("span").text();
 var inputValue = $(this).parent().siblings("textarea").val()
+var hourTimeSlot = $('#nine-container');
 
 function updateDay() {
     currentDate = moment().format('dddd, MMMM Do');
@@ -11,23 +12,34 @@ setInterval(updateDay, 1000);
 
 function saveTodoListItem(event) {
     event.preventDefault();
+
 };
 // on click ADD button - save to localstorage
 // event listner for all the add buttons
 $( ".btn-primary" ).on( "click", function() {
 
-    // grab value from input
     var inputValue = $(this).parent().siblings("textarea").val()
-    // grab time slot e.g. 9AM
     var timeSlot = $(this).parent().siblings("span").text();
 
     // localstorage save
     localStorage.setItem(timeSlot, inputValue);
-    localStorage.getItem(inputValue);
+
 });
 
+[...$('textarea')].forEach( function (element) {
 
+    var timeSlot = $(element).siblings("span").text();
+    var existingValue = localStorage.getItem(timeSlot);
+    element.value = existingValue;
+});
 
 // do this for all elements - 9AM - 5PM
 // grab value from localstorage
 // save it to input elements
+
+// function changeHourColor() {
+//   let hour = now.getHours();
+//   if (hour <= 9) {
+//       document.write(hourTimeSlot: background: green)
+//   }
+// }
